78e2d3e3fe411553db44148bb27625e178e86656
jchoponis <justin@choponis.us>
2022-11-16 21:26:44 -0500

    initial commit for section 16

---
+++ client/src/App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  // leave this out temporiarily until express is working, as it will fail cuz <App> will render Fib, which won't work right now
+  // render(<App />);
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+});
---
+++ k8s/secret-readme.md
@@ -0,0 +1,9 @@
+# imperative command
+
+https://www.udemy.com/course/docker-and-kubernetes-the-complete-guide/learn/lecture/11582326
+
+video 235
+
+```
+kubectl create secret generic <name> --from-literal <key=value>
+```
